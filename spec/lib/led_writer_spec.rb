f93ed3202e583500dd84c0dd548eb2da3fe1b2af
Julian Fischer <fischer@avarteq.de>
2016-01-01 15:51:25 +0100

    Adds sleep to see leds

--- spec/lib/led_writer_spec.rb
+++ spec/lib/led_writer_spec.rb
@@ -9,9 +9,11 @@ RSpec.describe LEDWriter do
   
   it "should write a white color" do
     expect(@writer.write([255,255,255])).to be true
+    sleep 2
   end
 
   it "should write a black color" do
     expect(@writer.write([0,0,0])).to be true
+    sleep 2
   end
 end
