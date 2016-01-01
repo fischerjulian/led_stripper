956e3ceb47c13e306e6ffbfb5e54c3372d2ef771
Julian Fischer <fischer@avarteq.de>
2016-01-01 16:01:24 +0100

    Adds soft pwm

--- lib/led_writer.rb
+++ lib/led_writer.rb
@@ -7,16 +7,19 @@ class LEDWriter
     @gpio_b = 24
 
     @io = WiringPi::GPIO.new do |gpio|
+      gpio.pwm_set_range(255)
+
       [17, 22, 24].each do |pin|
         gpio.pin_mode(pin, WiringPi::OUTPUT)      
+        gpio.soft_pwm_create(pin, 0, 255)
       end      
     end
   end
 
   def write(color = [255, 255, 255])
-    @io.pwm_write(@gpio_r, color[0])
-    @io.pwm_write(@gpio_g, color[1])
-    @io.pwm_write(@gpio_b, color[2])
+    @io.soft_pwm_write(@gpio_r, color[0])
+    @io.soft_pwm_write(@gpio_g, color[1])
+    @io.soft_pwm_write(@gpio_b, color[2])
     true
   end
 end
